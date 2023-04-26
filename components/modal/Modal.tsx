907914f91f6b7dacfaae313c0f2f8855d4266216
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-26 20:51:50 +0200

    Create modal component, matching created tests

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,16 +1,32 @@
-export const Modal = () => {
+import React, { ReactNode } from 'react';
 
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLDivElement;
+    target.id === 'wrapper' ? onClose() : null;
+  };
   return (
     <div
-      role="modal"
-      className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center "
-      id="wrapper"
-      >
-      <div className="container max-w-lg flex flex-col">
-        <div className="bg-filter-background p-2">
-         Modal Message
+      role='modal'
+      className='fixed inset-0 bg-filter-background bg-opacity-25 flex justify-center items-center'
+      id='wrapper'
+      onClick={handleClose}>
+      <div className='container max-w-lg flex flex-col'>
+        <div className='bg-filter-background p-2'>
+          {children}
         </div>
       </div>
     </div>
+
   );
 };
+
+
+
