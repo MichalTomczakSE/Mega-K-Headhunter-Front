d6d8e479074c9360e88a936d4320687de3e9d3b0
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-05-01 12:54:42 +0200

    Add labels for SalaryInput component

--- components/filter/SalaryInput.tsx
+++ components/filter/SalaryInput.tsx
@@ -17,7 +17,7 @@ export const SalaryInput = ({ onInputChange, filterTitle, onReset }: InputFormPr
 
   const resetInputs = () => {
     setInputValues({ min: '', max: '' });
-  }
+  };
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>, inputName: string) => {
     const newInputValues = { ...inputValues, [inputName]: event.target.value };
     setInputValues(newInputValues);
@@ -33,22 +33,36 @@ export const SalaryInput = ({ onInputChange, filterTitle, onReset }: InputFormPr
       <div
         className='flex mb-3'>
         <div className='flex flex-col w-3/4 sm:flex-row mr-2 my-2'>
-          <input
-            type='number'
-            value={inputValues.min}
-            min={0}
-            onChange={(event) => handleInputChange(event, 'min')}
-            className='mr-2 mb-2 sm:mb-0 px-3 py-2 text-xs bg-secondary-background'
-            placeholder='np. 1000zł'
-          />
-          <input
-            type='number'
-            min={inputValues.min}
-            value={inputValues.max}
-            onChange={(event) => handleInputChange(event, 'max')}
-            className='mr-2 px-3 py-2 text-xs bg-secondary-background'
-            placeholder='np. 10000zł'
-          />
+          <div
+          className="flex items-center">
+            <label
+              className='mr-3'>
+              Od
+            </label>
+            <input
+              type='number'
+              value={inputValues.min}
+              min={0}
+              onChange={(event) => handleInputChange(event, 'min')}
+              className='mr-2 mb-2 sm:mb-0 px-3 py-2 text-xs bg-secondary-background'
+              placeholder='np. 1000zł'
+            />
+          </div>
+          <div
+            className="flex items-center">
+            <label
+              className='mr-3'>
+              Do
+            </label>
+            <input
+              type='number'
+              min={inputValues.min}
+              value={inputValues.max}
+              onChange={(event) => handleInputChange(event, 'max')}
+              className='mr-2 px-3 py-2 text-xs bg-secondary-background'
+              placeholder='np. 10000zł'
+            />
+          </div>
         </div>
       </div>
     </>
