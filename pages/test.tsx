39635ce8c1d67d324a929e05b5553d6b09e10872
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-05-04 18:54:21 +0200

    Add components with different status for testing purposes

--- pages/test.tsx
+++ pages/test.tsx
@@ -5,7 +5,9 @@ const TestPage = ()=>  {
     <>
       <Header/>
       <section className="bg-primary-background h-[100vh]">
-        <StudentList/>
+        <StudentList status={1}/>
+        <hr/>
+        <StudentList status={2}/>
       </section>
     </>
   )
