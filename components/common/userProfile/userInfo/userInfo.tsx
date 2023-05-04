7d74a04414b02098c4b6c8c1ff3447c057b8501a
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-05-04 08:08:17 +0200

    Add tables with preferences and grades to user profile

--- components/common/userProfile/userInfo/userInfo.tsx
+++ components/common/userProfile/userInfo/userInfo.tsx
@@ -1,14 +1,31 @@
-import {InfoList} from "./infoList";
-import {UserInfoInterface} from "@/interfaces/user/userInterfaces";
+import { InfoList } from './infoList';
+import { UserInfoInterface } from '@/interfaces/user/userInterfaces';
+import { RatingForUser } from '@/components/common/rate/RatingTable';
+import { studentMock } from '@/components/common/student/StudentMock';
 
 interface PropsInterface {
-    data: UserInfoInterface
+  data: UserInfoInterface;
 }
 
-export const UserInfo=({data}:PropsInterface)=>{
-return (
+const student = studentMock[0];
+
+export const UserInfo = ({ data }: PropsInterface) => {
+  return (
     <>
-        <InfoList data={data}/>
+      <div>
+        <RatingForUser courseCompletion={student.grades.courseCompletion}
+                       courseEngagement={student.grades.courseEngagement}
+                       projectDegree={student.grades.projectDegree}
+                       teamProjectDegree={student.grades.teamProjectDegree}
+                       expectedTypeWork={student.expectedTypeWork}
+                       targetWorkCity={student.targetWorkCity}
+                       expectedSalary={student.expectedSalary}
+                       expectedContractType={student.expectedContractType}
+                       canTakeApprenticeship={student.canTakeApprenticeship}
+                       monthsOfCommercialExp={student.monthsOfCommercialExp} />
+
+      <InfoList data={data} />
+      </div>
     </>
-)
-}
+  );
+};
