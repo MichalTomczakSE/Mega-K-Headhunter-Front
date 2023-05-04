4cdf8dbd93bd05c1f9cf98ec9ecaf7b6cabeee08
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-05-04 18:48:53 +0200

    Change props and add classes for Button component

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,14 +1,18 @@
-interface ButtonProps { 
-children: string,
-  type?:"submit" | "reset",
-  color?:string,
-  onClick?:() => void,
+import { ReactNode } from 'react';
+
+interface ButtonProps {
+  children: ReactNode,
+  type?: 'submit' | 'reset',
+  color?: string,
+  onClick?: () => void,
+  className?: string,
 }
-export const Button = ({ children,type,color,onClick }: ButtonProps ) => {
+
+export const Button = ({ children, type, color, onClick, className }: ButtonProps) => {
   return (
     <button
-      type={type?type:"button"}
-      className={`${color?color:"bg-primary-red"} text-light-primary-text py-2 px-4`}
+      type={type ? type : 'button'}
+      className={`${color ? color : 'bg-primary-red'} ${className} text-light-primary-text py-2 px-4`}
       onClick={onClick}
     >
       {children}
