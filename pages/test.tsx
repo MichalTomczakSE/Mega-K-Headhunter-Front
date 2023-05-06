809af7143630f4082e18b6e18f367a9f61789801
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-05-07 00:07:39 +0200

    Add created component to test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react';
 import { Header } from '@/components/common/Header';
-import { FilterForm } from '@/components/filter/FilterForm';
-import { Button } from '@/components/common/Button';
+import { AccountDashboard } from '@/components/panels/AccountDashboard';
+import { ResetPassword } from '@/components/PasswordForms/ResetPassword';
 
 export default function TestPage() {
-  const [showModal, setShowModal] = useState(false);
-  const callback = (payload: boolean) => {
-    setShowModal(payload);
-  };
+
   return (
     <>
       <Header />
-      <Button
-        onClick={() => setShowModal(true)}>
-        Run Filter Form, Run!
-      </Button>
-      <section className='bg-primary-background h-[100vh]'>
-        <FilterForm modalStatus={showModal} callback={callback}/>
-      </section>
+      <AccountDashboard>
+        <ResetPassword />
+      </AccountDashboard>
     </>
   );
 }
