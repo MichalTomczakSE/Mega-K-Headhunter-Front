9964b6fc5bb5949b444f89744940839ba9401298
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-05-07 11:16:16 +0200

    Added more items to menu, define display depending on isAdmin prop

--- components/panels/SidebarMenu.tsx
+++ components/panels/SidebarMenu.tsx
@@ -1,5 +1,12 @@
 import { ListItem } from '@/components/panels/MenuElement';
-import { EmployedIcon, PasswordIcon, UsersIcon } from '@/components/icons/SVGIcons';
+import {
+  AccountInfoIcon,
+  AppointmentStudentsIcon,
+  DeleteAccountIcon,
+  EmployedIcon,
+  PasswordIcon,
+  UsersIcon,
+} from '@/components/icons/SVGIcons';
 
 interface SidebarMenuProps {
   isAdmin: boolean;
@@ -7,24 +14,56 @@ interface SidebarMenuProps {
 
 export const SidebarMenu = ({ isAdmin }: SidebarMenuProps) => {
   return (
-    <ul>
+
+    <ul
+    className=''>
       <ListItem urlAddress='/'>
         <div className='flex items-center'>
-          <UsersIcon height={16} width={16} />
+          <AccountInfoIcon height={16} width={16} />
           <p className='px-1'>
-            Dodaj użytkowników
+            Twoje dane
           </p>
         </div>
       </ListItem>
       {isAdmin &&
-        <ListItem urlAddress='/'>
-          <div className='flex items-center'>
-            <EmployedIcon height={16} width={16} />
-            <p className='px-1'>
-              Zatrudnieni studenci
-            </p>
-          </div>
-        </ListItem>
+        <>
+          <ListItem urlAddress='/'>
+            <div className='flex items-center'>
+              <UsersIcon height={16} width={16} />
+              <p className='px-1'>
+                Dodaj użytkowników
+              </p>
+            </div>
+          </ListItem>
+          <ListItem urlAddress='/'>
+            <div className='flex items-center'>
+              <EmployedIcon height={16} width={16} />
+              <p className='px-1'>
+                Zatrudnieni studenci
+              </p>
+            </div>
+          </ListItem>
+        </>
+      }
+      {!isAdmin &&
+        <>
+          <ListItem urlAddress='/'>
+            <div className='flex items-center'>
+              <AppointmentStudentsIcon height={16} width={16} />
+              <p className='px-1'>
+                Do rozmowy
+              </p>
+            </div>
+          </ListItem>
+          <ListItem urlAddress='/'>
+            <div className='flex items-center'>
+              <DeleteAccountIcon height={16} width={16} />
+              <p className='px-1'>
+                Usuń konto
+              </p>
+            </div>
+          </ListItem>
+        </>
       }
       <ListItem urlAddress='/'>
         <div className='flex items-center'>
